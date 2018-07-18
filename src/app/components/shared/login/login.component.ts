0f209ab5fc2af61c21dbab1abedc055137cc6bfc
stefanrosu <rosu.stefan96@gmail.com>
2018-07-18 15:53:30 +0300

    login,register,reset page design done

--- src/app/components/shared/login/login.component.ts
+++ src/app/components/shared/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  @ViewChild('login') loginForm: NgForm;
+  @ViewChild('reset') resetForm: NgForm;
+  @ViewChild('create') createForm: NgForm;
 
   constructor() { }
+
+  public loginContent = true;
+  public resetContent = false;
+  public registerContent = false;
 
   ngOnInit() {
   }
 
+  onSignin( ){
+    
+    console.log(this.loginForm.value);
+    this.loginForm.reset();
+    
+  }
+  onReset(){
+    console.log(this.resetForm);
+    
+  }
+
+  onRegister(){
+
+    const email = this.createForm.value.email_log;
+    const first_name = this.createForm.value.first_name;
+    const last_name = this.createForm.value.last_name;
+    const active = 2;
+    const user = [{
+      email: email,
+      name: first_name,
+      last_name: last_name,
+      active: active
+    }];
+
+    console.log(this.createForm);
+    console.log(user);
+  }
+  changeSignup(){
+    this.registerContent = true;
+    this.loginContent = false;
+  }
+  onResetPage(){
+    this.resetContent = true;
+    this.loginContent = false;
+    this.registerContent = false;
+  }
 }
--- src/app/services/api-connection/api-connection.service.ts
+++ src/app/services/api-connection/api-connection.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +10,14 @@ export class ApiConnectionService {
    * There's also a possibility to use a local BE, with json-server.
    */
 
-  constructor() { }
+  constructor(private http: Http) { }
 }
+// registerUser(user : User){
+//   const body: User = {
+//     password: user.password_register,
+//     email: user.Email,
+//     name: user.FirstName,
+//     last_ame: user.LastName
+//   }
+//   return this.http.post('http://localhost:3000/users', body);
+// }
