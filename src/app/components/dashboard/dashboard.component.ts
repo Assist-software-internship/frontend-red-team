94523e821df5c8419b31922b81032295d67bb7ea
manoleaioan <ioanmanolea@gmail.com>
2018-07-19 17:17:54 +0300

    dashboard admin

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; 
 
 @Component({
   selector: 'app-dashboard',
@@ -6,12 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  title = 'app works'
+  title = 'app works';
+
+  alertme(){
+    alert();
+  }
+
+
   constructor() { }
 
   ngOnInit() {
     
   }
   
-
 }
