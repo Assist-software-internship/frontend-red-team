e8545f6469df767a952621a96bb0087d5a41ad29
stefanrosu <rosu.stefan96@gmail.com>
2018-07-25 10:29:30 +0300

    added use hash to routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -63,7 +63,7 @@ const routes: Routes = [
     FormsModule,
     HttpModule,
     HttpClientModule,
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, {useHash: true}),
   ],
   providers: [ApiConnectionService],
   bootstrap: [AppComponent],
