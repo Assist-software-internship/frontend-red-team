e8a7108a99fcd0dee7832df320b6562d66b4f268
manoleaioan <ioanmanolea@gmail.com>
2018-07-26 17:54:50 +0300

    admin chapter

--- src/app/components/shared/chapter-list/chapter-list.component.ts
+++ src/app/components/shared/chapter-list/chapter-list.component.ts
@@ -3,6 +3,8 @@ import {NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 
+
+
 @NgModule({
 declarations:[ChapterListComponent],
 imports:[BrowserModule,FormsModule],
@@ -17,25 +19,30 @@ providers:[]
 export class ChapterListComponent implements OnInit {
   desc = " While saying goodbye means accepting that a part of our life is over, it also ... of that reality had started to creepinto my psyche weeks ago, when I was being fitted ... We journeyed together, watching each other grow through the innocence of "
   listCategory = [
-    { category: 'Astrology', id: 1, chapter:"I", title : 'Welcome to blablabla', shortdesc : this.desc },
-    { category: 'Astrology', id: 1, chapter:"II", title : 'Welcome to blablabla', shortdesc : "desc2" },
-    { category: 'Astrology', id: 1, chapter:"III", title : 'Welcome to blablabla', shortdesc :  this.desc  },
-    { category: 'Astrology', id: 1, chapter:"IV", title : 'Welcome to blablabla', shortdesc : "asdasdasdasdasdasd123123123" },
-    { category: 'Astrology', id: 1, chapter:"V", title : 'Welcome to blablabla', shortdesc :  this.desc  },
-    { category: 'Astrology', id: 1, chapter:"VI", title : 'Welcome to blablabla', shortdesc : "asdasdasdasdasdasd123123123" },
-    { category: 'Astrology', id: 1, chapter:"VII", title : 'Welcome to blablabla', shortdesc : "asdasdasdasdasdasd123123123" },
-    { category: 'Astrology', id: 1, chapter:"VIII", title : 'Welcome to blablabla', shortdesc :  this.desc  },
-    { category: 'Astrology', id: 1, chapter:"IX", title : 'Welcome to blablabla', shortdesc : "asdasdasdasdasdasd123123123" },
-    { category: 'Astrology', id: 1, chapter:"X", title : 'Welcome to blablabla', shortdesc : "asdasdasdasdasdasd123123123" },
+    { category: 'Astrology', id: 1, cat_id:2,chapter:"I", title : 'Welcome Ioan', shortdesc : this.desc },
+    { category: 'Astrology', id: 2, cat_id:2,chapter:"II", title : 'Welcome to blablabla', shortdesc : "desc2" },
+    { category: 'Astrology', id: 3, cat_id:2,chapter:"III", title : 'Welcome to blablabla', shortdesc :  this.desc  },
+    { category: 'Astrology', id: 4, cat_id:2,chapter:"IV", title : 'Welcome to blablabla', shortdesc : "asdasdasdasdasdasd123123123" },
+    { category: 'Astrology', id: 5, cat_id:2,chapter:"V", title : 'Welcome to blablabla', shortdesc :  this.desc  },
+    { category: 'Astrology', id: 6, cat_id:2,chapter:"VI", title : 'Welcome to blablabla', shortdesc : "asdasdasdasdasdasd123123123" },
+    { category: 'Astrology', id: 7, cat_id:2,chapter:"VII", title : 'Welcome to blablabla', shortdesc : "asdasdasdasdasdasd123123123" },
+    { category: 'Astrology', id: 8, cat_id:2,chapter:"VIII", title : 'Welcome to blablabla', shortdesc :  this.desc  },
+    { category: 'Astrology', id: 9, cat_id:2,chapter:"IX", title : 'Welcome to blablabla', shortdesc : "asdasdasdasdasdasd123123123" },
+    { category: 'Astrology', id: 10, cat_id:2,chapter:"X", title : 'Welcome to blablabla', shortdesc : "asdasdasdasdasdasd123123123" },
   ];
   filteredStatus = '';
-  max = 6;
+  max = 4;
   public account_role = 1;
 
-  public live_chapter
-  public live_title;
-  public live_desc;
-  public createVisible = false
+  public live_chapter = '';
+  public live_title = '';
+  public live_desc = '';
+
+  public t23= "";
+
+  public createVisible = false;
+  public editEnabled = false;
+  public deleteEnabled = false;
 
   public categoryTitle: string;
   public categorySubtitle: string;
@@ -56,18 +63,73 @@ export class ChapterListComponent implements OnInit {
   toggleCreate(){
    
     if(this.createVisible)
-      this.createVisible = false
+       this.createVisible = false
       else
-      this.createVisible=true
+        {this.createVisible=true
+        this.deleteEnabled = false;
+        this.editEnabled = false}
   }
 
   createNewCourse(){
-    this.listCategory.push({ category: 'Astrology', id: 200, chapter:this.live_chapter, title : this.live_title, shortdesc : this.live_desc});
+    if (this.live_title.length>0 || this.live_chapter.length>0  || this.live_desc.length>0 ){
+      this.listCategory.push({ category: 'Astrology', id: this.listCategory.length+1,cat_id:2, chapter:this.live_chapter, title : this.live_title, shortdesc : this.live_desc});
+      this.reset();
+    }
+  }
 
-    this.live_chapter
-    this.live_title;
-    this.live_desc;
-    this.createVisible = false
+  reset(){
+    this.live_chapter = '';
+    this.live_title = '';
+    this.live_desc = '';
+    this.createVisible = false;
+  }
+  cancel(){
+    this.reset();
+  }
+
+  toggleeditChapters(){
+    if(this.editEnabled)
+     this.editEnabled = false
+    else
+      this.editEnabled=true  
+      this.deleteEnabled = false;
+      this.createVisible=false;
+  }
+
+  toggledeleteChapters(){
+    if(this.deleteEnabled)
+    this.deleteEnabled = false;
+   else{
+    this.deleteEnabled=true  ;
+    this.editEnabled = false;
+    this.createVisible=false;
+   }
+
+  }
+
+  deletech(item){
+    for (let index = 0; index < this.listCategory.length; index++) {
+      const element = this.listCategory[index];
+      if(element.id==item.id){
+        this.listCategory.splice(this.listCategory.indexOf(element), 1); 
+      }
+    }
+    // this.toggledeleteChapters();
+  }
+
+  editCh(item){
+   
+    for (let index = 0; index < this.listCategory.length; index++) {
+      const element = this.listCategory[index];
+      if(element.id==item.id){
+        console.log(item)
+        this.listCategory[index] =  item
+        // this.listCategory.splice(this.listCategory.indexOf(element), 1); 
+      }
+    }
+
+    // this.listCategory[RevIndex] = { category: 'Astrology', id: 1,cat_id:2, chapter:"I", title : 'Welcome Spider', shortdesc : this.desc }
+    this.toggleeditChapters();
   }
 
 }
